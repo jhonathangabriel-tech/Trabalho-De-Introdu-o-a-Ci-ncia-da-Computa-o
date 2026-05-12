1e038ca7f1ecc6697d4016e4799ef94bd3f8aab3
jhonathangabriel-tech <jhonathan.gabriel@rede.ulbra.br>
2026-05-11 21:56:18 -0300

    Mudei o nome de algumas variaveis

--- codigo.py
+++ codigo.py
@@ -1,6 +1,6 @@
-sm = int(input('Qual é o seu salario minimo ?'))
+soma = int(input('Qual é o seu salario minimo ?'))
 
-q = int(input('Qual é aa quantidade de quilowatts que é gastada em sua residencia ?'))
+quilowatts = int(input('Qual é aa quantidade de quilowatts que é gastada em sua residencia ?'))
 
 
 sm2 = sm/2
