9cbfaaabb3053af96fbce8c4abb1e903693daf48
jhonathangabriel-tech <jhonathan.gabriel@rede.ulbra.br>
2026-05-11 21:54:44 -0300

    Dei um pouco de espaços entre as linhas do codigo

--- codigo.py
+++ codigo.py
@@ -2,7 +2,9 @@ sm = int(input('Qual é o seu salario minimo ?'))
 
 q = int(input('Qual é aa quantidade de quilowatts que é gastada em sua residencia ?'))
 
+
 sm2 = sm/2
+
 ql = sm2*q
 
 print('O valor a pagar em sua residencia é ',ql)
